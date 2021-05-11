c6c0e0cd2d5fd666362c7e5904a3a76f807bca91
Andrew Grant <andrewgrant243@gmail.com>
2021-05-11 09:13:30 -0400

    Automate creating a user that can access member accounts

---
+++ bootstrap/create_iam_admin_user.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+set -e
+
+PASSWORD=$1
+USER_NAME=AdminUser
+
+aws iam create-user --user-name $USER_NAME
+aws iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --user-name $USER_NAME
+
+output=$(aws iam create-login-profile --user-name $USER_NAME --password "$PASSWORD")
+echo "$output"
