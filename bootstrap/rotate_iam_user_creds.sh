ed79ec36f33e7b550e465719718c111e5c1a83e1
Andrew Grant <andrewgrant243@gmail.com>
2021-05-15 15:16:44 -0400

    Make sure env vars are set

--- bootstrap/rotate_iam_user_creds.sh
+++ bootstrap/rotate_iam_user_creds.sh
@@ -30,5 +30,17 @@ export AWS_SECRET_ACCESS_KEY=$(echo $new_access_key | jq -r '.SecretAccessKey')
 echo $AWS_ACCESS_KEY_ID
 echo $AWS_SECRET_ACCESS_KEY
 
+if [ -z "$AWS_ACCESS_KEY_ID" ]
+then
+      echo "\$AWS_ACCESS_KEY_ID is not set"
+      exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]
+then
+      echo "\$AWS_SECRET_ACCESS_KEY is empty"
+      exit 1
+fi
+
 travis encrypt AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
 travis encrypt AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
