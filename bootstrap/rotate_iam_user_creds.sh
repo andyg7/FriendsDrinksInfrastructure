2cbdc2a19305f32dd5d20e350ca2bb6af91919dd
Andrew Grant <andrewgrant243@gmail.com>
2021-05-15 14:49:47 -0400

    Actually add script to rotate creds

---
+++ bootstrap/rotate_iam_user_creds.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu
+
+while true; do
+  aws iam list-access-keys --user-name CloudFormationDeployerUser --max-items 1 --query 'AccessKeyMetadata[0].AccessKeyId' --output text
+  access_key_id=$(aws iam list-access-keys --user-name CloudFormationDeployerUser --max-items 1 --query 'AccessKeyMetadata[0].AccessKeyId' --output text | grep -v 'None' || true)
+  echo "Access key id is $access_key_id - about to delete it"
+  if [ -z "$access_key_id" ]
+  then
+    break
+  else
+    aws iam update-access-key --user-name CloudFormationDeployerUser --access-key-id "$access_key_id" --status Inactive
+    echo "Made $access_key_id inactive"
+    aws iam delete-access-key --user-name CloudFormationDeployerUser --access-key-id  $access_key_id
+    echo "Deleted $access_key_id"
+  fi
+done
+
+echo 'Done deleting access keys'
+
+new_access_key=$(aws iam create-access-key --user-name CloudFormationDeployerUser | jq -r '.AccessKey')
+echo "New access key $new_access_key"
+
+aws secretsmanager --region us-east-1 update-secret --secret-id CloudFormationDeployerUserCredentials --secret-string "$new_access_key"
+
+travis encrypt AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+travis encrypt AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
